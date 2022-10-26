f8bc866af52c210a16cef2044ebb5f19ca9f1461
AljonLaxamana <114331118+AljonLaxamana@users.noreply.github.com>
2022-10-26 10:35:57 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,227 @@
+let homeScore = document.getElementById("home-score")
+let awayScore = document.getElementById("away-score")
+let btnStatus = document.getElementById("btn-start")
+const PAUSE_GAME = "Pause Game";
+let homeScoring = 0
+let awayScoring = 0
+
+// Home functions
+
+function addPointHome(score, team) {
+    let game = btnStatus.textContent === PAUSE_GAME;
+    let clock = btnStatus.textContent !== PAUSE_GAME ? shotClock.value : 24;
+
+    if (score == 1 && team == "home") {
+        homeScoring += 1;
+        homeScore.textContent = homeScoring;
+    }
+    else if (score == 2 && team == "home") {
+        homeScoring += 2;
+        homeScore.textContent = homeScoring;
+    }
+    else if (score == 3 && team == "home") {
+        homeScoring += 3;
+        homeScore.textContent = homeScoring;
+    }
+
+    shotclockreset(clock, game)
+}
+
+
+function subtractPointHome(score, team) {
+    let tempScore = homeScoring
+    if (score == -1 && team == "home" && homeScoring > 0) {
+        homeScoring -= 1;
+    }
+    else if (score == -2 && team == "home" && homeScoring > 0) {
+        homeScoring -= 2;
+    }
+    else if (score == -3 && team == "home" && homeScoring > 0) {
+        homeScoring -= 3;
+    }
+    else {
+        window.alert("Score is already equal to 0 can't decrement points")
+        homeScoring = 0
+        homeScore.textContent = 0
+    }
+    if (homeScoring < 0) {
+        window.alert("The score will be negative")
+        homeScoring = tempScore
+    }
+    homeScore.textContent = homeScoring;
+}
+
+// Away Functions
+function addPointAway(score, team) {
+    let game = btnStatus.textContent === PAUSE_GAME;
+    let clock = btnStatus.textContent !== PAUSE_GAME ? shotClock.value : 24;
+
+    if (score == 1 && team == "away") {
+        awayScoring += 1;
+        awayScore.textContent = awayScoring;
+    }
+    else if (score == 2 && team == "away") {
+        awayScoring += 2;
+        awayScore.textContent = awayScoring;
+    }
+    else if (score == 3 && team == "away") {
+        awayScoring += 3;
+        awayScore.textContent = awayScoring;
+    }
+
+    shotclockreset(clock, game)
+}
+
+
+function subtractPointAway(score, team) {
+    let tempScore = awayScoring
+    if (score == -1 && team == "home" && awayScoring > 0) {
+        awayScoring -= 1;
+    }
+    else if (score == -2 && team == "home" && awayScoring > 0) {
+        awayScoring -= 2;
+    }
+    else if (score == -3 && team == "home" && awayScoring > 0) {
+        awayScoring -= 3;
+    }
+    else {
+        window.alert("Score is already equal to 0 can't decrement points")
+        awayScoring = 0
+        awayScore.textContent = 0
+    }
+    if (awayScoring < 0) {
+        window.alert("The score will be negative")
+        awayScoring = tempScore
+    }
+    awayScore.textContent = awayScoring;
+}
+
+// Reset
+
+function resetScore(team) {
+    if (team === "home") {
+        homeScoring = 0
+        homeScore.textContent = 0
+    }
+    else if (team === "away") {
+        awayScoring = 0
+        awayScore.textContent = 0
+    }
+
+}
+
+
+function changeBtnStatus() {
+    let buttonAddTrigger = document.getElementsByClassName("btn-add-points")
+    let buttonSubtractTrigger = document.getElementsByClassName("btn-subtract-points")
+    let buttonReset = document.getElementsByClassName("btn-reset-score")
+    let clock = 24
+    let game = true;
+
+    for (let i = 0; i < buttonAddTrigger.length; i++) {
+        buttonAddTrigger[i].removeAttribute("disabled");
+    }
+
+    for (let i = 0; i < buttonReset.length; i++) {
+        buttonReset[i].removeAttribute("disabled");
+    }
+
+    if (btnStatus.textContent === "Start Game") {
+        btnStatus.textContent = "Pause Game"
+        clock = shotClock.value
+        qtrTimer();
+    }
+    else if (btnStatus.textContent === "Pause Game") {
+        clock = shotClock.value;
+        game = false;
+        btnStatus.textContent = "Resume Game"
+        for (let i = 0; i < buttonSubtractTrigger.length; i++) {
+            buttonSubtractTrigger[i].removeAttribute("disabled", true);
+        }
+    }
+    else {
+        for (let i = 0; i < buttonSubtractTrigger.length; i++) {
+            buttonSubtractTrigger[i].setAttribute("disabled", true);
+        }
+        for (let i = 0; i < buttonReset.length; i++) {
+            buttonReset[i].setAttribute("disabled", true);
+        }
+        btnStatus.textContent = "Pause Game"
+        clock = shotClock.value;
+        game = true;
+    }
+    if (btnStatus.textContent === "Pause Game") {
+        btnStatus.style.color = 'red'
+
+    }
+    else {
+        btnStatus.style.color = 'green'
+    }
+
+    shotclockreset(clock, game)
+}
+
+let minutesElem = document.getElementById("minutes")
+let secondsElem = document.getElementById("seconds")
+let millisecondsElem = document.getElementById("milliseconds")
+let shotClock = document.getElementById("shotclock-seconds")
+let interval = null
+let qtrInterval = null
+
+function qtrTimer() {
+    let minutes = minutesElem.value
+    let seconds = secondsElem.value
+    let milliseconds = millisecondsElem.value
+
+
+    qtrInterval = setInterval(function () {
+        milliseconds--
+        millisecondsElem.value = milliseconds
+
+        if (milliseconds < 0) {
+            milliseconds = 99
+            seconds--
+            secondsElem.value = seconds
+        }
+        if (seconds < 0) {
+            seconds = 59
+            secondsElem.value = seconds
+            minutes--
+            minutesElem.value = minutes
+        }
+        if (minutes < 0) {
+            clearInterval(qtrInterval)
+            alert("End of Quarter")
+            minutesElem.value = 12
+            secondsElem.value = 0
+            milliseconds.value = 0
+            btnStatus.click()
+        }
+    }, 10)
+
+
+}
+
+function shotclocktimer() {
+    shotClock.value--
+
+    if (shotClock.value < 0) {
+        btnStatus.click();
+        shotClock.value = 24
+    }
+}
+
+function shotclockreset(clock, game) {
+    clearInterval(interval)
+    shotClock.value = clock
+
+    if (game)
+        interval = setInterval(shotclocktimer, 1000)
+}
+
+function shotClockRestrict() {
+    if (shotClock.value > 24 || shotClock.value <= 0) {
+        alert("Shotclock is only 24 seconds")
+        shotClock.value = 24
+    }
+}
